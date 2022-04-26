c2c8855a9ef778d3e8162b2cf6ce763c97fbfbb8
Erik <d21erisu@student.his.se>
2022-04-26 15:52:23 +0200

    added recyclerwievadapter and recyclerviewholder

--- app/src/main/java/com/example/networking/MountainAdapter.java
+++ app/src/main/java/com/example/networking/MountainAdapter.java
@@ -1,9 +1,44 @@
 package com.example.networking;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
 import java.util.List;
 
-public class MountainAdapter {
-    public MountainAdapter(List<String> mountains) {
+public class MountainAdapter extends RecyclerView.Adapter<MountainAdapter.MountainViewHolder>{
+    private List<String> mountains;
 
+    public MountainAdapter(List<String> mountains) {
+        this.mountains = mountains;
+    }
+
+    @NonNull
+    @Override
+    public MountainViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item,parent,false);
+        return new MountainViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MountainViewHolder holder, int position) {
+        holder.mountain_name.setText(mountains.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mountains.size();
+    }
+
+    public class MountainViewHolder extends RecyclerView.ViewHolder {
+        private TextView mountain_name;
+        public MountainViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mountain_name = itemView.findViewById(R.id.mountain_name);
+        }
     }
 }
