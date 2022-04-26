9b9c4f16dd3712b8002552781f76a4bb15ea085d
Erik <d21erisu@student.his.se>
2022-04-26 15:56:17 +0200

    added recyclerwievadapter and recyclerviewholder

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -4,10 +4,17 @@ import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
+    private RecyclerView recyclerView;
 
     private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
     private final String JSON_FILE = "mountains.json";
@@ -17,7 +24,15 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        List<String> mountains = Arrays.asList("Test", "Test2");
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView = findViewById(R.id.recycler_view);
+        MountainAdapter mountainAdapter = new MountainAdapter(mountains);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+
         new JsonFile(this, this).execute(JSON_FILE);
+
+
     }
 
     @Override
